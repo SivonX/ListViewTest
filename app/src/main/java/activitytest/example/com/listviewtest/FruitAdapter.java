4484703b637182bf3da85b9e297d817b1df2b0eb
SivonX <2500998085@qq.com>
2018-04-15 15:23:17 +0800

    xxx

--- app/src/main/java/activitytest/example/com/listviewtest/FruitAdapter.java
+++ app/src/main/java/activitytest/example/com/listviewtest/FruitAdapter.java
@@ -16,20 +16,19 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
 
     private int resourceId;
 
-    public FruitAdapter(Context context, int textViewResourceId, List<Fruit>objects) {
-        super(context,textViewResourceId,objects);
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
         resourceId = resourceId;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        Fruit fruit=getItem(position);
-        @SuppressLint("ViewHolder") View view= LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-        ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
-        TextView fruitName=(TextView)view.findViewById(R.id.fruit_name);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        @SuppressLint("ViewHolder") View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
         fruitImage.setImageResource(fruit.getImageId());
         fruitName.setText(fruit.getName());
         return view;
-    }
     }
 }
